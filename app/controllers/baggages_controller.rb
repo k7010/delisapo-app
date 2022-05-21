ad226414abc8448be129cab803c56cb280a3d4ea
k7010 <103634772+k7010@users.noreply.github.com>
2022-05-21 12:36:44 +0900

    持出登録

--- app/controllers/baggages_controller.rb
+++ app/controllers/baggages_controller.rb
@@ -7,4 +7,20 @@ class BaggagesController < ApplicationController
   def new
     @baggage = Baggage.new
   end
+
+  def create
+    @baggage = Baggage.new(baggage_params)
+    if @baggage.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def baggage_params
+    params.require(baggage).permit(:address_id, :building, :block, :family_name, :first_name)
+  end
 end
