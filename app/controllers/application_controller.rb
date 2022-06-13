f99965ec7c4f64a5bceefc40f5ebf2c959a71d6d
k7010 <103634772+k7010@users.noreply.github.com>
2022-06-13 17:49:51 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
 
   private
   def configure_permitted_parameters
@@ -8,9 +7,4 @@ class ApplicationController < ActionController::Base
                                       keys: [:staff_number])
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '1234'
-    end
-  end
 end
